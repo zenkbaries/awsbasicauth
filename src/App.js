ea03d7a1923ec6ec59a7af5e210709327125c70a
John Towery <kbaries@gmail.com>
2020-11-08 15:05:32 -0700

    add V2 message

--- src/App.js
+++ src/App.js
@@ -9,12 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. V2
         </p>
-        {/* <button onClick={() => Auth.signIn()}>Sign In</button> */}
-        <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
-        <button onClick={Auth.checkUser}>Check User</button>
-        <button onClick={Auth.signOut}>Sign Out</button>
+        
 
       </header>
     </div>
