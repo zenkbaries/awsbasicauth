6a6beb2f6245486ac4fa425eb3a466fc90681b08
John Towery <kbaries@gmail.com>
2020-11-08 14:41:04 -0700

    initial commit to upload repo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Auth } from 'aws-amplify';
+
 function App() {
   return (
     <div className="App">
@@ -9,14 +11,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <button onClick={() => Auth.signIn()}>Sign In</button> */}
+        <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+        <button onClick={Auth.checkUser}>Check User</button>
+        <button onClick={Auth.signOut}>Sign Out</button>
+
       </header>
     </div>
   );
